7cc0c33858b76dcfcd9e86e2ed35b360f80d1967
kristen <32827781+wogeliusk@users.noreply.github.com>
2021-04-18 01:02:58 -0500

    Initial with button click

--- App.js
+++ App.js
@@ -1,16 +1,52 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, {Component} from 'react'
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+  } from 'react-native'
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
-}
+  class App extends Component {
+    state = {
+      count: 0
+    }
+    onPress = () => {
+      this.setState({
+        count: this.state.count + 1
+      })
+    }
+    render(){
+      return (
+        <View style={styles.container}>
+          <TouchableOpacity
+          style={styles.button}
+          onPress={this.onPress}
+          >
+           <Text style={styles.text}>Add</Text>
+
+          </TouchableOpacity>
+                 
+        </View>
+      )
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+      color: 'white'
+    },
+    button: {
+      alignItems: 'center',
+      backgroundColor: '#16DB93',
+      borderRadius: 8,
+      marginBottom: 10,
+      padding: 10,
+    }
+  })
+
+  export default App;
